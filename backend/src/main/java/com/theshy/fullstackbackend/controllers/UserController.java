bed935eb2a68c6a62da3d7d3e4e796ca1d8e7b16
tienv1per <tienv1perds@gmail.com>
2023-06-28 16:13:40 +0700

    add UPDATE/DELETE user

--- backend/src/main/java/com/theshy/fullstackbackend/controllers/UserController.java
+++ backend/src/main/java/com/theshy/fullstackbackend/controllers/UserController.java
@@ -29,4 +29,26 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@PathVariable Long id, @RequestBody User newUser) {
+        return userRepository.findById(id)
+                .map((user)-> {
+                    user.setName(newUser.getName());
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        else {
+            userRepository.deleteById(id);
+            return "User with id: " + id + " has been deleted";
+        }
+    }
 }
